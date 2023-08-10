b684a5b034b95421b0cae73c10b58c513d74eca7
Maxwell-ssl <129470588+Maxwell-ssl@users.noreply.github.com>
2023-08-10 12:17:00 -0300

    Add files via upload

---
+++ javascript.1/script.js
@@ -0,0 +1,10 @@
+function sumar() 
+{
+
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+
+    alert("El resultado de la suma es: " + resultado);
+}
+
